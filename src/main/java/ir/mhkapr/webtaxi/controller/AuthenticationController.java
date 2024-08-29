75496abae2a5a5fa2fe28e0c60cc4eda1d4601c0
MHKAPR <119296844+MhkaPR@users.noreply.github.com>
2024-08-29 17:25:10 +0330

    register method of controller wrote

---
+++ src/main/java/ir/mhkapr/webtaxi/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package ir.mhkapr.webtaxi.controller;
+
+import ir.mhkapr.webtaxi.DTOs.AuthenticationResponse;
+import ir.mhkapr.webtaxi.DTOs.RegisterRequest;
+import ir.mhkapr.webtaxi.excepption.UserAlreadyExistsException;
+import ir.mhkapr.webtaxi.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.management.RuntimeErrorException;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(RegisterRequest request) throws UserAlreadyExistsException {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+}
