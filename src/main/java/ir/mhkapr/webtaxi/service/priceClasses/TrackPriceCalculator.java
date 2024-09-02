5b55651c49269e07ce60a133b8860ca662f3e47a
MHKAPR <119296844+MhkaPR@users.noreply.github.com>
2024-09-02 21:13:02 +0330

    some change for do good calculating price

--- src/main/java/ir/mhkapr/webtaxi/service/priceClasses/TrackPriceCalculator.java
+++ src/main/java/ir/mhkapr/webtaxi/service/priceClasses/TrackPriceCalculator.java
@@ -1,5 +1,7 @@
 package ir.mhkapr.webtaxi.service.priceClasses;
+import org.springframework.stereotype.Component;
 
+@Component("TRACK")
 public class TrackPriceCalculator implements CalculatorPrice{
     @Override
     public Double calculatePrice(Double distance) {
